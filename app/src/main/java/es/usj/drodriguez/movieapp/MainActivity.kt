5ce1159f673b07961988111d463146edc98669d5
Diego <jdrr1998@hotmail.com>
2021-04-25 11:59:27 +0200

    Sync commit

--- app/src/main/java/es/usj/drodriguez/movieapp/MainActivity.kt
+++ app/src/main/java/es/usj/drodriguez/movieapp/MainActivity.kt
@@ -6,18 +6,47 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.CheckBox
 import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.toolbar_main, menu)
+
         val searchMovieList =  menu?.findItem(R.id.btn_search)?.actionView as SearchView
         searchMovieList.queryHint = getString(R.string.searchMovie)
+
+        val fav = menu.findItem(R.id.btn_fav)?.actionView as CheckBox
+        fav.buttonDrawable  =  ContextCompat.getDrawable(this,R.drawable.outline_favorite_border_24)
+        fav.isChecked = false
         return super.onCreateOptionsMenu(menu)
        }
 
+    override fun onOptionsItemSelected(item: MenuItem) :Boolean{
+        when (item.itemId) {
+            R.id.btn_fav -> {
+                val fav = item as CheckBox
+                if (fav.isChecked) {
+                    fav.buttonDrawable = ContextCompat.getDrawable(this, R.drawable.outline_favorite_border_24)
+                } else {
+                    fav.buttonDrawable = ContextCompat.getDrawable(this, R.drawable.outline_favorite_24)
+                }
+                fav.isChecked = !fav.isChecked
+                return true
+            }
+            R.id.btn_contact -> {
+                startActivity(Intent(this, Contact::class.java))
+                return true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
+    }
     private val adapter by lazy { ViewPagerAdapter(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,15 +70,6 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar_movieList)
         toolbar_movieList.showOverflowMenu()
-        toolbar_movieList.setOnMenuItemClickListener {
-            when (it.itemId){
-                R.id.btn_contact -> {
-                    startActivity(Intent(this, Contact::class.java))
-                    true
-                }
-                else -> false
-            }
-        }
 
         btn_addMovie.shrink()
         btn_addMovie.setOnClickListener {
